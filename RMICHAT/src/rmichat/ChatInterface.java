d9d1db283231c94bfc2528e8bb183cb32ddbeee6
Alexy Cruz <alexyc@unitec.edu>
2018-12-11 22:37:19 -0600

    Testing RMI Chat in two different machines

--- RMICHAT/src/rmichat/ChatInterface.java
+++ RMICHAT/src/rmichat/ChatInterface.java
@@ -10,10 +10,14 @@ package rmichat;
  * @author xioma
  */
 import java.rmi.*;
- 
-public interface ChatInterface extends Remote{
-	public String getName() throws RemoteException;
-	public void send(String msg) throws RemoteException;
-	public void setClient(ChatInterface c)throws RemoteException;
-	public ChatInterface getClient() throws RemoteException;
+
+public interface ChatInterface extends Remote {
+
+    public String getName() throws RemoteException;
+
+    public void send(String msg) throws RemoteException;
+
+    public void setClient(ChatInterface c) throws RemoteException;
+
+    public ChatInterface getClient() throws RemoteException;
 }
--- RMICHAT/src/rmichat/ChatServer.java
+++ RMICHAT/src/rmichat/ChatServer.java
@@ -14,29 +14,35 @@ import java.rmi.registry.LocateRegistry;
 import java.rmi.registry.Registry;
 import java.rmi.server.*;
 import java.util.*;
- 
+
 public class ChatServer {
-public static void main (String[] argv) {
-    try {	    	
-	    	Scanner s=new Scanner(System.in);
-	    	System.out.println("Nombre: ");
-	    	String name=s.nextLine().trim();
- 
-	    	Chat server = new Chat(name);	
-	    	Registry registry = LocateRegistry.createRegistry(8888);
-                registry.bind("ejemplo", server);
- 
-	    	while(true){
-	    		String msg=s.nextLine().trim();
-	    		if (server.getClient()!=null){
-	    			ChatInterface client=server.getClient();
-	    			msg="["+server.getName()+"] "+msg;
-	    			client.send(msg);
-	    		}	
-	    	}
- 
-    	}catch (Exception e) {
-    		System.out.println("[System] Server failed: " + e);
-    	}
-	}
+
+    public static void main(String[] argv) {
+        try {
+            Scanner s = new Scanner(System.in);
+            System.out.println("Nombre: ");
+            String name = s.nextLine().trim();
+
+            Chat server = new Chat(name);
+            
+            //Public address: 181.115.9.201
+            //hostname ip-Adress: 127.0.1.1
+            
+            System.setProperty("java.rmi.server.hostname", "192.168.x.x");
+            Registry registry = LocateRegistry.createRegistry(1099);
+            registry.bind("ejemplo", server);
+
+            while (true) {
+                String msg = s.nextLine().trim();
+                if (server.getClient() != null) {
+                    ChatInterface client = server.getClient();
+                    msg = "[" + server.getName() + "] " + msg;
+                    client.send(msg);
+                }
+            }
+
+        } catch (Exception e) {
+            System.out.println("[System] Server failed: " + e);
+        }
+    }
 }
